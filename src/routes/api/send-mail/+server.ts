993475995422eec9422de743d9b29501e1c07802
Thai Radin <thairadin@outlook.com>
2024-12-24 16:07:28 -0500

    almost working...

--- src/routes/api/send-mail/+server.ts
+++ src/routes/api/send-mail/+server.ts
@@ -7,8 +7,28 @@ import { json } from "@sveltejs/kit";
 // }
 
 export async function POST({ request }) {
-    const data = await request.json();
-    console.log(data);
+    const { contactMail, contactName, informationAboutProject } = await request.json();
+
+    if (!contactMail || !contactName || !informationAboutProject) {
+        json({ message: "Could not send email. Missing data." }, { status: 400 });
+    }
+
+    const message = {
+        to: "thai@techrealm.io",
+        from: "thai@techrealm.io",
+        subject: "Contact Form on your Portfolio",
+        html: `Somebody used the contact form on your site. <br/>
+        Name: ${contactName},
+        Email: ${contactMail},
+        Information about the project: ${informationAboutProject}`,
+    };
+
+    try {
+        await sgMail.send(message);
+        return json({ emailSentSuccessfully: true });
+    } catch (err) {
+        return json({ err }, { status: 500 });
+    }
 
     // await delay(2000);
 
